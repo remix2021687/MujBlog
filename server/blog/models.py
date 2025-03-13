42906bb827d3d00be5a2cb6c7159f0ce51a088f2
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2025-03-13 11:02:18 +0100

    backend update

--- server/blog/models.py
+++ server/blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100, blank=False, null=False)
     photo = models.ImageField(upload_to='uploads/photos', blank=False, null=False)
-    display_description = models.TextField(max_length=500, blank=False, null=False)
+    display_description = models.CharField(default="", max_length=150, blank=False, null=False)
     text = models.TextField(blank=False, null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now=True, editable=False)
