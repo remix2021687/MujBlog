749840c0bf285550e06203485b11dcb1ccc9f6ca
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2025-03-15 20:56:33 +0100

    Zoom on photo and disable browser zoom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,23 @@
+import { useEffect } from "react"
+
 export const App = ({ Route, Components }) => {
+    useEffect(() => {
+        document.addEventListener('wheel', (event) => {
+            if (event.ctrlKey) {
+                event.preventDefault();
+            }
+        }, {passive: false})
+
+        return () => {
+            document.removeEventListener('wheel', (event) => {
+                if (event.ctrlKey) {
+                    event.preventDefault();
+                }
+            })
+        }
+    }, [])
+
+
     return (
         <Route Components={Components} />
     )
--- client/src/Components/GalleryPage/components/GalleryPhoto/components/GalleryPhotoModel.jsx
+++ client/src/Components/GalleryPage/components/GalleryPhoto/components/GalleryPhotoModel.jsx
@@ -1,6 +1,20 @@
 import { motion } from "motion/react"
+import { useState } from "react";
 
 export const GalleryPhotoModel = ({ id, photo, onClose}) => {
+    const [zoomImg, setZoomImg] = useState(1);
+
+    const onWheelScroll = (event) => {
+
+        if (event.ctrlKey) {
+            const delta = event.deltaY
+            if (delta < 0) {
+                setZoomImg(zoomImg + 0.2)
+            } else {
+                setZoomImg(zoomImg - 0.2)
+            }
+        }
+    }
     
     return (
         <motion.section
@@ -8,14 +22,23 @@ export const GalleryPhotoModel = ({ id, photo, onClose}) => {
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             exit={{opacity: 0}}
-            onClick={onClose}
+
+            onWheel={(event) => {
+                onWheelScroll(event)
+            }}
+            
         >
+            <button onClick={onClose}><span>Close</span></button>   
             <motion.section 
                 className="GalleryPhotoModel_bg"
                 layoutId={id}
             >
                 <motion.img 
+                    drag
+                    dragElastic={0.2}
                     src={photo}
+                    animate={{scale: zoomImg}}
+                    
                 />
             </motion.section>
         </motion.section>
