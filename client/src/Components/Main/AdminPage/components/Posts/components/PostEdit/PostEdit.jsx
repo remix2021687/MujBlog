d00adfc9ac8356205f214698c77efff49a4e6a43
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2025-04-04 23:14:48 +0200

    Modal beta

--- client/src/Components/Main/AdminPage/components/Posts/components/PostEdit/PostEdit.jsx
+++ client/src/Components/Main/AdminPage/components/Posts/components/PostEdit/PostEdit.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { PostEditDropmenu } from './components/PostEditDropmenu';
 import { DeletePostAdmin } from '../../../../../../../Axios/AxiosInit';
+import { ConfirmDelateModal } from './components/ConfirmDelateModal';
 
 
 export const PostEdit = ({ id, index, Name, Photo, DisplayDescription, Text, postChild }) => {
@@ -38,33 +39,36 @@ export const PostEdit = ({ id, index, Name, Photo, DisplayDescription, Text, pos
     }
 
     return (
-        <motion.section 
-            className="PostEdit"
-            variants={postChild}
-            custom={index}
-        >
-            <section className='PostEdit_header'>
-                <section className='PostEdit_header_photo_name'>
-                    <img src={Photo} alt='photo' />
-                    <h2>{Name}</h2>
+        <>
+            <ConfirmDelateModal />
+            <motion.section 
+                className="PostEdit"
+                variants={postChild}
+                custom={index}
+            >
+                <section className='PostEdit_header'>
+                    <section className='PostEdit_header_photo_name'>
+                        <img src={Photo} alt='photo' />
+                        <h2>{Name}</h2>
+                    </section>
+                    <section className='PostEdit_header_control'>
+                        <button className='edit' onClick={() => setIsOpen(true)}>Edit</button>
+                        <button className='delete' onClick={onDelete} >Delete</button>
+                    </section>
                 </section>
-                <section className='PostEdit_header_control'>
-                    <button className='edit' onClick={() => setIsOpen(true)}>Edit</button>
-                    <button className='delete' onClick={onDelete} >Delete</button>
-                </section>
-            </section>
-            <AnimatePresence>
-                {
-                    isOpen && <PostEditDropmenu 
-                        Id={id}
-                        Name={Name}
-                        Photo={Photo}
-                        DisplayDescription={DisplayDescription}
-                        Text={Text}
-                        setCloseEdit={CloseHendle}
-                    />
-                }
-            </AnimatePresence>
-        </motion.section>
+                <AnimatePresence>
+                    {
+                        isOpen && <PostEditDropmenu 
+                            Id={id}
+                            Name={Name}
+                            Photo={Photo}
+                            DisplayDescription={DisplayDescription}
+                            Text={Text}
+                            setCloseEdit={CloseHendle}
+                        />
+                    }
+                </AnimatePresence>
+            </motion.section>
+        </>
     )
 }
---
+++ client/src/Components/Main/AdminPage/components/Posts/components/PostEdit/components/ConfirmDelateModal.jsx
@@ -0,0 +1,18 @@
+export const ConfirmDelateModal = () => {
+    return (
+        <>
+            <section className="ConfirmDelateModal">
+                <section className="ConfirmDelateModal_modal">
+                    <section className="ConfirmDelateModal_modal_header">
+                        <h2>Confirm to delete post ?</h2>
+                        <p></p>
+                    </section>
+                    <section className="ConfirmDelateModal_modal_button">
+                        <button>Delete</button>
+                        <button>Cancel</button>
+                    </section>
+                </section>
+            </section>
+        </>
+    )
+}
