ab25e2e91f88b3bcf62e4ac45a8a2bae28d3f645
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2025-04-05 09:37:55 +0200

    refactoring logic

--- client/src/Components/Main/AdminPage/components/Posts/components/PostEdit/PostEdit.jsx
+++ client/src/Components/Main/AdminPage/components/Posts/components/PostEdit/PostEdit.jsx
@@ -1,13 +1,12 @@
 import { motion, AnimatePresence } from 'motion/react';
 import { useState } from 'react';
-import { toast } from 'react-toastify';
 import { PostEditDropmenu } from './components/PostEditDropmenu';
-import { DeletePostAdmin } from '../../../../../../../Axios/AxiosInit';
 import { ConfirmDelateModal } from './components/ConfirmDelateModal';
 
 
 export const PostEdit = ({ id, index, Name, Photo, DisplayDescription, Text, postChild }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isOnDelete, setIsOnDelete] = useState(false);
 
     const CloseHendle = (event) => {
         if (event) {
@@ -15,32 +14,21 @@ export const PostEdit = ({ id, index, Name, Photo, DisplayDescription, Text, pos
         }
     }
 
-    const onDelete = () => {
-        DeletePostAdmin(id)
-        .then(() => {
-            toast.success('Delete Post is successfull !', {
-                position: 'top-right',
-                closeOnClick: false,
-                draggable: true,
-                pauseOnHover: false,
-                autoClose: 3000,
-            })
-        })
-
-        .catch(() => {
-            toast.error("Delete Post isn't successfull !", {
-                position: 'top-right',
-                closeOnClick: false,
-                draggable: true,
-                pauseOnHover: false,
-                autoClose: 3000,
-            })
-        })
+    const CancelHendle = (event) => {
+        if (event) {
+            setIsOnDelete(false);
+        }
     }
+
+    const onDelete = () => setIsOnDelete(true);
 
     return (
         <>
-            <ConfirmDelateModal />
+            <AnimatePresence>
+                {
+                    isOnDelete && <ConfirmDelateModal id={id} name={Name}  cancelState={CancelHendle}/>
+                }
+            </AnimatePresence>
             <motion.section 
                 className="PostEdit"
                 variants={postChild}
@@ -52,8 +40,30 @@ export const PostEdit = ({ id, index, Name, Photo, DisplayDescription, Text, pos
                         <h2>{Name}</h2>
                     </section>
                     <section className='PostEdit_header_control'>
-                        <button className='edit' onClick={() => setIsOpen(true)}>Edit</button>
-                        <button className='delete' onClick={onDelete} >Delete</button>
+                        <motion.button 
+                            className='edit' 
+                            onClick={() => setIsOpen(true)}
+                            whileHover={{
+                                color: '#ffffff',
+                                backgroundColor: '#ffa500',
+                                boxShadow: '0px 0px 10px 7px rgba(0,0,0,0.12)'
+                            }}
+                            whileTap={{scale: 0.9}}
+                        >
+                            Edit
+                        </motion.button>
+                        <motion.button 
+                            className='delete' 
+                            onClick={onDelete}
+                            whileHover={{
+                                color: '#ffffff',
+                                backgroundColor: '#ff0000',
+                                boxShadow: '0px 0px 10px 7px rgba(0,0,0,0.12)'
+                            }}
+                            whileTap={{scale: 0.9}}
+                        >
+                            Delete
+                        </motion.button>
                     </section>
                 </section>
                 <AnimatePresence>
--- client/src/Components/Main/AdminPage/components/Posts/components/PostEdit/components/ConfirmDelateModal.jsx
+++ client/src/Components/Main/AdminPage/components/Posts/components/PostEdit/components/ConfirmDelateModal.jsx
@@ -1,18 +1,131 @@
-export const ConfirmDelateModal = () => {
+import { motion } from "motion/react"
+import { toast } from 'react-toastify';
+import { DeletePostAdmin } from "../../../../../../../../Axios/AxiosInit"
+
+export const ConfirmDelateModal = ({ id, name, cancelState }) => {
+
+    const ConfirmDelateModalParent = {
+        open: {
+            opacity: 1,
+
+            transition: {
+                duraction: 0.5,
+                staggerChildren: 0.08,
+                delayChildren: 0.1
+            }
+        },
+
+        close: {
+            opacity: 0,
+        }
+    }
+
+    const ConfirmDelateModalPerChild = {
+        open: {
+            opacity: 1,
+            y: 0,
+
+            transition: {
+                duraction: 0.5,
+                staggerChildren: 0.08,
+                delayChildren: 0.1
+            }
+        },
+
+        close: {
+            opacity: 0,
+            y: -15,
+
+        }
+    }
+
+    const ConfirmDelateModalChild = {
+        open: {
+            opacity: 1,
+            y: 0,
+        },
+
+        close: {
+            opacity: 0,
+            y: -15,
+
+        }
+    }
+
+    const DeleteBtn = () => {
+        DeletePostAdmin(id)
+        .then(() => {
+            toast.success('Delete Post is successfull !', {
+                position: 'top-right',
+                closeOnClick: false,
+                draggable: true,
+                pauseOnHover: false,
+                autoClose: 3000,
+            })
+
+            cancelState(true)
+        })
+
+        .catch(() => {
+            toast.error("Delete Post isn't successfull !", {
+                position: 'top-right',
+                closeOnClick: false,
+                draggable: true,
+                pauseOnHover: false,
+                autoClose: 3000,
+            })
+        })
+    }
+
     return (
         <>
-            <section className="ConfirmDelateModal">
-                <section className="ConfirmDelateModal_modal">
+            <motion.section 
+                className="ConfirmDelateModal"
+                variants={ConfirmDelateModalParent}
+                initial={"close"}
+                animate={"open"}
+                exit={"close"}
+
+            >
+                <motion.section 
+                    className="ConfirmDelateModal_modal"
+                    variants={ConfirmDelateModalPerChild}
+                >
                     <section className="ConfirmDelateModal_modal_header">
-                        <h2>Confirm to delete post ?</h2>
-                        <p></p>
+                        <motion.h2 variants={ConfirmDelateModalChild}>Confirm to delete post ?</motion.h2>
+                        <motion.p variants={ConfirmDelateModalChild}>Вы дейстивтельно хотите удалит <b>{name}</b> ?</motion.p>
                     </section>
                     <section className="ConfirmDelateModal_modal_button">
-                        <button>Delete</button>
-                        <button>Cancel</button>
+                        <motion.button 
+                            variants={ConfirmDelateModalChild} 
+                            className="del"
+                            whileHover={{
+                                scale: 1.05,
+                                backgroundColor: '#ff0000',
+                                color: '#ffffff'
+                            }}
+                            whileTap={{scale: 0.9}}
+                            onClick={DeleteBtn}
+                            
+                        >
+                            Delete
+                        </motion.button>
+                        <motion.button 
+                            variants={ConfirmDelateModalChild}
+                            className="cancel"
+                            whileHover={{
+                                scale: 1.05,
+                                backgroundColor: '#008000',
+                                color: '#ffffff'
+                            }}
+                            whileTap={{scale: 0.9}}
+                            onClick={() => cancelState(true)}
+                        >
+                            Cancel
+                        </motion.button>
                     </section>
-                </section>
-            </section>
+                </motion.section>
+            </motion.section>
         </>
     )
 }
