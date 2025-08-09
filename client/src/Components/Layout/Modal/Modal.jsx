af950ddf792ea8cb96e05f07e9ff6865b2a71473
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2025-08-09 22:19:44 +0200

    Bug fix and re-staling on Hello Kitty

--- client/src/Components/Layout/Modal/Modal.jsx
+++ client/src/Components/Layout/Modal/Modal.jsx
@@ -1,123 +1,122 @@
-import { motion } from "motion/react"
-import { useEffect } from "react"
-import moment from "moment/moment"
-import { useGetPostSelfQuery, useGetPostPinSelfQuery } from "../../../redux/slices/api/PostsSlice"
+import { motion } from "motion/react";
+import { useEffect } from "react";
+import moment from "moment/moment";
+import {
+	useGetPostSelfQuery,
+	useGetPostPinSelfQuery,
+} from "../../../redux/slices/api/PostsSlice";
 
-export const Modal = ({id, setReturnEvent, pinMode}) => {
-    const { data: postData, isError: postDataError } = useGetPostSelfQuery(id);
-    const { data: postDataPin, isError: isPinError } = useGetPostPinSelfQuery(id);
-    const formateData = moment(
-        pinMode ? postDataPin?.date_created : postData?.date_created,
-    ).format("D.MM.YYYY")
-    
-    const ModalAnimation = {
-        open: {
-            width: "95%",
-            height: "95%",
-            y: 0,
+export const Modal = ({ id, setReturnEvent, pinMode }) => {
+	const { data: postData, isError: postDataError } = useGetPostSelfQuery(id);
+	const { data: postDataPin, isError: isPinError } = useGetPostPinSelfQuery(id);
+	const formateData = moment(
+		pinMode ? postDataPin?.date_created : postData?.date_created
+	).format("D.MM.YYYY");
 
-            transition: {
-                when: "beforeChildren",
-                duraction: 2.5,
-                staggerChildren: 0.1,
-            }
-        },
-        close: {
-            width: "5%",
-            height: "5%",
-            y: -100,
+	const ModalAnimation = {
+		open: {
+			width: "95%",
+			height: "95%",
+			y: 0,
 
-            transition: {
-                duraction: 2.5,
-                staggerChildren: 0.1,
-                when: "afterChildren",
-            }
-        },
+			transition: {
+				when: "beforeChildren",
+				duraction: 2.5,
+				staggerChildren: 0.1,
+			},
+		},
+		close: {
+			width: "5%",
+			height: "5%",
+			y: -100,
 
-    }
+			transition: {
+				duraction: 2.5,
+				staggerChildren: 0.1,
+				when: "afterChildren",
+			},
+		},
+	};
 
-    const ModalAnimationChildren = {
-        open: {
-            y: 0,
-            opacity: 1,
-            
-        },
-        close: {
-            y: -15,
-            opacity: 0,
-        }
-    }
+	const ModalAnimationChildren = {
+		open: {
+			y: 0,
+			opacity: 1,
+		},
+		close: {
+			y: -15,
+			opacity: 0,
+		},
+	};
 
-    const ClickHendler = (event) => {
-        setReturnEvent(event)
-    }
+	const ClickHendler = (event) => {
+		setReturnEvent(event);
+	};
 
-    useEffect(() => {
-        if (postDataError) {
-            setReturnEvent(false);
-        }
+	useEffect(() => {
+		console.log(postDataError, isPinError);
+	}, [postDataError, isPinError]);
 
-    }, [postDataError])
+	return (
+		<>
+			<motion.section
+				className='Modal'
+				variants={ModalAnimation}
+				initial='close'
+				animate='open'
+				exit='close'
+				transition={{
+					ease: "easeInOut",
+				}}>
+				<section className='Modal_left'>
+					<motion.img
+						variants={ModalAnimationChildren}
+						src={pinMode ? postDataPin?.photo : postData?.photo}
+					/>
+					<section className='Modal_left_header'>
+						<motion.h1 variants={ModalAnimationChildren}>
+							{pinMode ? postDataPin?.name : postData?.name}
+						</motion.h1>
 
-    return (
-        <>
-            <motion.section 
-                className="Modal"
-                variants={ModalAnimation}
-                initial='close'
-                animate='open'
-                exit='close'
-                transition={{
-                    ease:'easeInOut'
-                }}
-            >
-                <section className="Modal_left">
-                    <motion.img variants={ModalAnimationChildren} src={
-                        pinMode ? postDataPin?.photo: postData?.photo
-                    } 
-                    />
-                    <section className="Modal_left_header">
-                        <motion.h1 variants={ModalAnimationChildren}>
-                            {pinMode ? postDataPin?.name : postData?.name}
-                        </motion.h1>
-
-                        <motion.p variants={ModalAnimationChildren}>
-                            {pinMode ? postDataPin?.text : postData?.text}
-                        </motion.p>
-                    </section>
-                </section>
-                <section className="Modal_right">
-                    <motion.section variants={ModalAnimationChildren} className="Modal_right_presonal">
-                        <section>
-                            <h1>Auhor by</h1>
-                            <h3>
-                                {
-                                    pinMode ? postDataPin?.author.last_name : postData?.author.last_name
-                                } 
-                                {
-                                    pinMode ? postDataPin?.author.first_name : postData?.author.first_name
-                                }
-                            </h3>
-                        </section>
-                        <motion.button 
-                            variants={ModalAnimationChildren} 
-                            onClick={ClickHendler}
-                            whileHover={{
-                                backgroundColor: "#ff0000",
-                                borderColor: "#ff0000",
-                                color: '#ffffff',
-                            }}
-                            whileTap={{
-                                scale: 0.9
-                            }}
-                        >
-                        Close
-                        </motion.button>
-                    </motion.section>
-                    <motion.h4 variants={ModalAnimationChildren}><span>Created by</span> {formateData}</motion.h4>
-                </section>
-            </motion.section>
-
-        </>
-    )
-}
+						<motion.p variants={ModalAnimationChildren}>
+							{pinMode ? postDataPin?.text : postData?.text}
+						</motion.p>
+					</section>
+				</section>
+				<section className='Modal_right'>
+					<motion.section
+						variants={ModalAnimationChildren}
+						className='Modal_right_presonal'>
+						<section>
+							<h1>Auhor by</h1>
+							<h3>
+								{pinMode
+									? postDataPin?.author.last_name
+									: postData?.author.last_name}
+								{pinMode
+									? postDataPin?.author.first_name
+									: postData?.author.first_name}
+							</h3>
+						</section>
+						<motion.button
+							variants={ModalAnimationChildren}
+							onClick={ClickHendler}
+							whileHover={{
+								backgroundColor: "#ff0000",
+								borderColor: "#ff0000",
+								color: "#ffffff",
+							}}
+							whileTap={{
+								scale: 0.9,
+							}}>
+							Close
+						</motion.button>
+					</motion.section>
+					<motion.h4 variants={ModalAnimationChildren}>
+						<span>Created by</span> {formateData}
+					</motion.h4>
+				</section>
+			</motion.section>
+		</>
+	);
+};
--- client/src/Components/Main/HomePage/components/PinPost/PinPost.jsx
+++ client/src/Components/Main/HomePage/components/PinPost/PinPost.jsx
@@ -1,33 +1,28 @@
-import { BlogBox } from "../../../../Layout/BlogBox/BlogBox"
+import { BlogBox } from "../../../../Layout/BlogBox/BlogBox";
 import { useGetPostPinQuery } from "../../../../../redux/slices/api/PostsSlice";
 
 export const PinPost = () => {
-    const { data: pinData, isLoading } = useGetPostPinQuery();
+	const { data: pinData, isLoading } = useGetPostPinQuery();
 
-    return (
-        <>
-            {
-                pinData?.length > 0 ?
-                <section className="PinPost">
-                    <h1>Favorite Posts</h1>
-                    <section className="PinPost_content">
-                        {
-                            pinData?.map((data, index) => 
-                                <BlogBox
-                                    key={index + 1}
-                                    id={data.id}
-                                    name={data.name}
-                                    image={data.photo}
-                                    display_description={data.display_description}
-                                    pinMode={true}
-                                />
-                            )
-                        }
-                    </section>
-                </section>
-                :
-                null
-            }
-        </>
-    )
-}
+	return (
+		<>
+			{pinData?.length > 0 ? (
+				<section className='PinPost'>
+					<h1>Favorite Posts</h1>
+					<section className='PinPost_content'>
+						{pinData?.map((data, index) => (
+							<BlogBox
+								key={index + 1}
+								id={data.id}
+								name={data.name}
+								image={data.photo}
+								display_description={data.display_description}
+								pinMode={true}
+							/>
+						))}
+					</section>
+				</section>
+			) : null}
+		</>
+	);
+};
