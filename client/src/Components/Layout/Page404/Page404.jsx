fb167947b4a88d16eaf4fc1366811db2a77fa6c9
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-07-29 16:21:47 +0200

    Update Page404

--- client/src/Components/Layout/Page404/Page404.jsx
+++ client/src/Components/Layout/Page404/Page404.jsx
@@ -1,16 +1,20 @@
-import { NavLink } from "react-router"
-import Logo from '../../../assets/img/kludieLogo.png'
-
+import { NavLink } from "react-router";
+import Logo from "../../../assets/img/mybloglogo.svg";
 
 export const Page404 = () => {
-    return (
-        <section className="Page404">
-            <img src={Logo} alt="logo"/>
-            <section className="Page404_header">
-                <h1>Page not found</h1>
-                <h3>Return to home page</h3>
-            </section>
-            <NavLink to={'/'}><span>Return</span></NavLink>
-        </section>
-    )
-}
+	return (
+		<section className="Page404">
+			<img
+				src={Logo}
+				alt="logo"
+			/>
+			<section className="Page404_header">
+				<h1>Page not found</h1>
+				<h3>Return to home page</h3>
+			</section>
+			<NavLink to={"/"}>
+				<span>Return</span>
+			</NavLink>
+		</section>
+	);
+};
